208cba307a5a5353e67277b4393fc1c917c32d18
KaitoKunTatsu <88390464+KaitoKunTatsu@users.noreply.github.com>
2021-11-05 20:02:21 +0100

    play 2 fix

--- LeJOS-Ev3/LeJOS-Projekt/src/de/server/CustomThread.java
+++ LeJOS-Ev3/LeJOS-Projekt/src/de/server/CustomThread.java
@@ -14,16 +14,15 @@ public class CustomThread extends Thread
 	// Klasse einer eigenen Bibliothek zum Umgang mit Arrays
 	ArrayUtils arrUt = new ArrayUtils();
 	
-	int counter = 1, angel;
+	int counter = 1, angle;
 	String button;
     EV3LargeRegulatedMotor motorA = new EV3LargeRegulatedMotor(MotorPort.A);
     EV3LargeRegulatedMotor motorB = new EV3LargeRegulatedMotor(MotorPort.B);    
     boolean recording = false;
     int recVal = 0;
-    boolean playing = false;
-    String[] recArr = new String[20];
-    long[] recTime = new long[20];
-    
+    String[] recArr = new String[10];
+    long[] recTime = new long[10];
+    int[] recAngle = new int[10];
     // Gyro Sensor + Messung initialisieren
     EV3GyroSensor gyro = new EV3GyroSensor(SensorPort.S1);
     SampleProvider gyroProvider = gyro.getAngleAndRateMode();
@@ -31,7 +30,7 @@ public class CustomThread extends Thread
     float[] tempSample = new float[gyroProvider.sampleSize()];
     
     // Methode zur Rotation in entgegengesetzte Richtung zum übergebenen Motor
-    public void rotate(RegulatedMotor motor, boolean back) 
+    public void rotate(RegulatedMotor motor, boolean back, int angle) 
     {
     	// Start des übergebenen Motors
     	motor.setSpeed(300);
@@ -41,22 +40,18 @@ public class CustomThread extends Thread
         // Gyro Messung vor der Drehung
     	gyroProvider.fetchSample(gyroSample, 0);
     	gyroProvider.fetchSample(tempSample, 0);
-    	// Wiederholung bis "angel" 90° von der Messung zuvor entfernt ist
-    	while ((tempSample[0] - gyroSample[0])*(tempSample[0] - gyroSample[0]) < (angel)*(angel)) 
+    	// Wiederholung bis der Winkel des EV3 den Wert von "angle" von der Messung zuvor entfernt ist
+    	while ((tempSample[0] - gyroSample[0])*(tempSample[0] - gyroSample[0]) < (angle-3)*(angle-3)) 
     	{
     		gyroProvider.fetchSample(tempSample, 0); 	
     	}
-    	// Abbremsen und stoppen 
-    	if (!back) {motor.backward();}
-    	else {motor.forward();}
-    	Delay.msDelay(80);
         motor.stop(false);
     }
     
     
     // Methode zum ausführen von Bewegungen
     // move: String = auszuführende Aktion als Text
-    private void movement(String move, boolean back) 
+    private void movement(String move, boolean back, int angle) 
     {
         switch(move) 
         {
@@ -68,12 +63,12 @@ public class CustomThread extends Thread
         // Drehung nach links
         case "LEFT":
             motorB.stop(false);
-            rotate(motorA, back);
+            rotate(motorA, back, angle);
             break;
         // Drehung nach rechts
         case "RIGHT":
             motorA.stop(false);
-            rotate(motorB, back);
+            rotate(motorB, back, angle);
             break;
         // geradeaus fahren
         case "UP":
@@ -114,48 +109,31 @@ public class CustomThread extends Thread
     // backwards: boolean = Soll das pattern erneut abgespielt oder zurück gefahren werden?
     private void play(boolean backwards) 
     {
-    	playing = true;
-    	// wenn die Strecke erneut gefahren werden soll
-	    if (!backwards) 
-	    {
-			try 
-	    	{
-				// Iteration des aufgenommenen Arrays 
-	    		for (int i = 0; i < recArr.length; i++) 
-		        {
-		            if (recArr[i] == "" || recArr[i] == null) break;
-		            System.out.println("playin... " + recArr[i]);
-		            movement(recArr[i], false);
-		            if (recArr[i] == "UP" || recArr[i] == "DOWN") { Delay.msDelay(recTime[i+1]-recTime[i]); }
-		        }
-		    } catch (Exception e) {System.out.println("Error");}
-	    }
-    	// wenn die Strecke zurück bis zum Anfangspunkt gefahren werden soll
-	    else 
-	    {
-	    	try 
-	    	{
-				// Iteration des aufgenommenen Arrays
-	    		for (int i = recArr.length-1; i >= 0; i--) 
-		        {
-		            if (recArr[i] == "" || recArr[i] == null) continue;
-		            System.out.println("playin... ");
-		            if (recArr[i] == "DOWN" || recArr[i] == "UP") 
-		            {
-		            	movement(recArr[i], true);
-		            	Delay.msDelay(recTime[i+1]-recTime[i]);
-		            	movement("ENTER", true);
-		            	continue;
-		            }
-		            movement(recArr[i], true);
-		        }
-		    } catch (Exception e) {System.out.println("Error");}
-	    }
+		int c;
+		if (backwards) { c = recArr.length-1; }
+		else { c = 0; }
+		    
+		try {
+		    while(c >= 0 && c < recArr.length) 
+		    {
+		    	if (backwards) 
+			    { 
+			    	if (recArr[c] != null) movement(recArr[c], true, recAngle[c]); 
+			    	if ("UP".equals(recArr[c]) || "DOWN".equals(recArr[c])) { Delay.msDelay(recTime[c+1]-recTime[c]); }
+			    	c--;
+			    }
+			    else 
+			    {
+			    	if (recArr[c] != null) movement(recArr[c], false, recAngle[c]);
+			    	if ("UP".equals(recArr[c]) || "DOWN".equals(recArr[c])) { Delay.msDelay(recTime[c+1]-recTime[c]); }
+			    	c++;
+			    }       	
+		    }
+    	} 
+    	catch (Exception e) { System.out.println("Error"); }
+	    movement("ENTER", false, 0);
 		System.out.println("FINISHED");
-        // Variablen auf den Stand vor der Aufnahme zurücksetzen 
 		recording = false;
-        recVal = 0;
-        playing = false;
     }
 
     // Main des Threads
@@ -163,16 +141,12 @@ public class CustomThread extends Thread
     {
         while(true) 
         {
-    		// Entspricht der Input des Servers dem zuvoriegen, wird var "button" geleert 
-    		if (Integer.parseInt(Server.inputButton.split(";")[1]) <= counter) 
-        	{
-        		button = ""; 
-        		continue;
-        	}
-        	// Kann aus dem Server des EV3 ein neuer Input erkannt werden, wird dieser auf var "button" übertragen
+    		// Entspricht der Input des Servers dem zuvoriegen, überspirnge
+    		if (Integer.parseInt(Server.inputButton.split(";")[1]) <= counter) continue;
+        	// Kann aus dem Server des EV3 ein neuer Input erkannt werden, wird dieser auf die Variable "button" übertragen
         	else 
         	{
-        		angel = Integer.parseInt(Server.inputButton.split(";")[2]);
+        		angle = Integer.parseInt(Server.inputButton.split(";")[2]);
         		button = Server.inputButton.split(";")[0];
         		counter++;
         	}
@@ -181,8 +155,7 @@ public class CustomThread extends Thread
         	// Reaktion auf den übergebenen Button
     		switch(button) 
         	{
-    		// Ist button leer, beende switch
-        	case "":
+    		case "":
         		break;
         	// Aufnahme beenden
         	case "STOPREC":
@@ -216,19 +189,21 @@ public class CustomThread extends Thread
     			{	
             		if (recVal > recArr.length-1) 
             		{
-            			recArr = arrUt.addElementToArray(recArr, button);
-            			recTime = arrUt.addElementToArray(recTime, System.currentTimeMillis());
+            			arrUt.addElementToArray(recArr, button);
+            			arrUt.addElementToArray(recAngle, angle);
+            			arrUt.addElementToArray(recTime, System.currentTimeMillis());
             		}
             		else 
             		{
             			recArr[recVal] = button;
+            			recAngle[recVal] = angle;
                         recTime[recVal] = System.currentTimeMillis();
             		}
             		recVal++;
                     System.out.println(button + " recorded");
     			}
     			// Bewegung ausführen
-        		movement(button, false);    
+        		movement(button, false, angle);
         	}
         }
    }
